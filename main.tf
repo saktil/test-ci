ab0895469cb7ccf7830368636d2f391e85cc2a87
saktil <leosaragih001@gmil.com>
2022-07-15 13:07:28 +0700

    test-2

--- main.tf
+++ main.tf
@@ -3,6 +3,18 @@ resource "aws_vpc" "my_vpc" {
 
   tags = {
 
+    edit                 = "leo"
+    env                  = "master"
+    git_commit           = "a23c01dff2cd47df15f6323d80808d7ad0f13449"
+    git_file             = "main.tf"
+    git_last_modified_at = "2022-07-15 05:49:33"
+    git_last_modified_by = "leosaragih001@gmil.com"
+    git_modifiers        = "leosaragih001"
+    git_org              = "saktil"
+    git_repo             = "test-ci"
+    team_ownership       = "sre-intern"
+    upload-to-gcb        = "cloud-build"
+    yor_trace            = "22c0e5cc-60ad-4217-8d92-706b536395fe"
   }
 }
 
@@ -13,6 +25,18 @@ resource "aws_subnet" "my_subnet" {
 
   tags = {
 
+    edit                 = "leo"
+    env                  = "master"
+    git_commit           = "a23c01dff2cd47df15f6323d80808d7ad0f13449"
+    git_file             = "main.tf"
+    git_last_modified_at = "2022-07-15 05:49:33"
+    git_last_modified_by = "leosaragih001@gmil.com"
+    git_modifiers        = "leosaragih001"
+    git_org              = "saktil"
+    git_repo             = "test-ci"
+    team_ownership       = "sre-intern"
+    upload-to-gcb        = "cloud-build"
+    yor_trace            = "87256e52-bab9-4634-ac2b-5f311b40e261"
   }
 }
 
@@ -22,6 +46,18 @@ resource "aws_network_interface" "sample" {
 
   tags = {
 
+    edit                 = "leo"
+    env                  = "master"
+    git_commit           = "a23c01dff2cd47df15f6323d80808d7ad0f13449"
+    git_file             = "main.tf"
+    git_last_modified_at = "2022-07-15 05:49:33"
+    git_last_modified_by = "leosaragih001@gmil.com"
+    git_modifiers        = "leosaragih001"
+    git_org              = "saktil"
+    git_repo             = "test-ci"
+    team_ownership       = "sre-intern"
+    upload-to-gcb        = "cloud-build"
+    yor_trace            = "21b3935d-700d-44bb-a3af-043350d5fee1"
   }
 }
 
@@ -35,6 +71,18 @@ resource "aws_instance" "sample" {
   }
   tags = {
 
+    edit                 = "leo"
+    env                  = "master"
+    git_commit           = "a23c01dff2cd47df15f6323d80808d7ad0f13449"
+    git_file             = "main.tf"
+    git_last_modified_at = "2022-07-15 05:49:33"
+    git_last_modified_by = "leosaragih001@gmil.com"
+    git_modifiers        = "leosaragih001"
+    git_org              = "saktil"
+    git_repo             = "test-ci"
+    team_ownership       = "sre-intern"
+    upload-to-gcb        = "cloud-build"
+    yor_trace            = "e53ecbd8-5352-4012-bd19-f70a736e950e"
   }
 
 }
